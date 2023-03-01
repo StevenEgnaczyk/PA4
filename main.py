ba6b7f5183860de573563358b9c5fc5ba36fda1e
Steven Egnaczyk <stevenegnaczyk@gmail.com>
2023-02-28 21:37:21 -0500

    uhhhh

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
 from clingo import Control
 from clingo import SymbolType
 
+def readASP(filename):
+    ctl = Control(["0"])
+    ctl.load(filename)
+    ctl.ground([("base", [])])
+    returnDict = {}
+    with ctl.solve(yield_=True) as hnd:
+        for m in hnd:
+            for term in m.symbols(shown=True):
+                argList = []
+                for a in term.arguments:
+                    if a.type == SymbolType.String:
+                        argList.append(a.string)
+                    if a.type == SymbolType.Number:
+                        argList.append(a.number)
+                    if a.type == SymbolType.Function:
+                        argList.append(a.name)
+                returnDict[term] = argList
+
+    return returnDict
 
 class KBAgent:
 
     @staticmethod
     def recieveAndSavePrecepts():
-        print("Penis")
+
+        info = readASP('AgentPosition.gr')
+        # xPos = argList[0]
+        # yPos = argList[1]
+
 
     @staticmethod
     def decideOnNextAction():
@@ -15,28 +38,9 @@ class KBAgent:
 
 if __name__ == "__main__":
 
-    ctl = Control(["0"])
-    ctl.load('prgm1.lp')
-    ctl.ground([("base", [])])
-    cnt=0
-    with ctl.solve(yield_=True) as hnd:
-        for m in hnd:
-            print('Model' + str(cnt))
-            for term in m.symbols(shown=True):
-                argList=[]
-                for a in term.arguments:
-                    if a.type == SymbolType.String:
-                        argList.append(a.string)
-                    if a.type == SymbolType.Number:
-                        argList.append(a.number)
-                    if a.type == SymbolType.Function:
-                        argList.append(a.name)
-                print(term.name, argList)
-
     WumpusAgent = KBAgent
+    while True:
 
-    # while True:
-
-        # WumpusAgent.recieveAndSavePrecepts()
-        # WumpusAgent.decideOnNextAction()
+        WumpusAgent.recieveAndSavePrecepts()
+        WumpusAgent.decideOnNextAction()
 
