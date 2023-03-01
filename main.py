f9abf9b72363dae829ffbd8bc754a961797648f6
zzteddy <edted22@gmail.com>
2023-03-01 16:55:25 -0500

    asd

--- main.py
+++ main.py
@@ -49,43 +49,6 @@ def printWorld(dict,agent):
     
     print(np.fliplr(np.rot90(np.flipud(np.matrix(twoDimArr)))))
 
-
-
-def generatePrecepts(agentInfo, worldInfo):
-
-    xPos = agentInfo['agent'][0][0]
-    yPos = agentInfo['agent'][0][1]
-
-    playerLocation = (xPos, yPos)
-    wumpusStenches = []
-    pitBreezes = []
-
-    gold = False
-
-    for key, value in worldInfo.items():
-        if key == "wumpus":
-            for coordinate in value:
-                wumpusStenches.append((coordinate[0]+1, coordinate[1]))
-                wumpusStenches.append((coordinate[0]-1, coordinate[1]))
-                wumpusStenches.append((coordinate[0], coordinate[1]+1))
-                wumpusStenches.append((coordinate[0], coordinate[1]-1))
-        if key == "pit":
-            for coordinate in value:
-                pitBreezes.append((coordinate[0]+1, coordinate[1]))
-                pitBreezes.append((coordinate[0]-1, coordinate[1]))
-                pitBreezes.append((coordinate[0], coordinate[1]+1))
-                pitBreezes.append((coordinate[0], coordinate[1]-1))
-        if key == "gold":
-            for coordinate in value:
-                if coordinate[0] == playerLocation[0] and coordinate[1] == playerLocation[1]:
-                    gold = True
-
-    stench = wumpusStenches.__contains__(playerLocation)
-    breeze = pitBreezes.__contains__(playerLocation)
-
-    return stench, breeze, gold
-
-
 class KBAgent:
 
     @staticmethod
